e4d9cb8fa8f52702066c8306bafc0e612f6e89e7
DESKTOP-4H2CDN2\Admin <xuanhoang.ks6@gmail.com>
2018-06-20 11:41:40 +0700

    xong bài 5

--- TeduShop.Model/Models/Slide.cs
+++ TeduShop.Model/Models/Slide.cs
@@ -1,11 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using TeduShop.Model.Abstract;
 
 namespace TeduShop.Model.Models
 {
-    [Table("Pages")]
-    public class Page : Auditable
+    [Table("Slides")]
+    public class Slide
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -15,11 +14,17 @@ namespace TeduShop.Model.Models
         [MaxLength(256)]
         public string Name { get; set; }
 
-        [Column(TypeName = "varchar")]
         [MaxLength(256)]
-        [Required]
-        public string Alias { get; set; }
+        public string Description { get; set; }
 
-        public string Content { get; set; }
+        [MaxLength(256)]
+        public string Image { get; set; }
+
+        [MaxLength(256)]
+        public string Url { get; set; }
+
+        public int? DisplayOrder { get; set; }
+
+        public bool Status { get; set; }
     }
 }
