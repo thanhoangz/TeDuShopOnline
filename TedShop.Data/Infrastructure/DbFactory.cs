1347e9aa954e610b25a40f1981cb233c84eda95d
DESKTOP-4H2CDN2\Admin <xuanhoang.ks6@gmail.com>
2018-06-20 15:27:20 +0700

    done lesson 6. the test commit

---
+++ TedShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace TedShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeDuShopDbContext dbContext;
+
+        public TeDuShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeDuShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ TedShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TedShop.Data;
+
+namespace TedShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private TeDuShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public TeDuShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+
+
+}
